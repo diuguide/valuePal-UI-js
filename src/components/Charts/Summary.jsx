c82f72bc52033fcfae7c198681d568e3f9a0847b
diuguide <everett.diuguid@gmail.com>
2021-10-03 22:41:08 -0400

    sunday commit

--- src/components/Charts/Summary.jsx
+++ src/components/Charts/Summary.jsx
@@ -1,8 +1,22 @@
 import { Row, Col } from "react-bootstrap";
 import Chart from "react-apexcharts";
 import { timeConvertArray } from "../../utilities/stockData";
+import { useEffect, useRef } from "react";
+import { useSelector } from "react-redux";
+import { dataState } from "../../slice/data/dataSlice";
 
 const Summary = ({ stockData }) => {
+  console.log("Stock data: ", stockData);
+  const data = useSelector(dataState);
+  const chartRef = useRef();
+
+  useEffect(() => {
+    chartRef.current = filterData(stockData);
+  }, [stockData]);
+
+  const filterData = (value) => {
+    return data.data.filter(el => el.exchange == value);
+  }
   const chartState = {
     options: {
       chart: {
@@ -18,30 +32,30 @@ const Summary = ({ stockData }) => {
         axisBorder: {
           show: false,
         },
-        categories: timeConvertArray(stockData.timestamp),
+        categories: timeConvertArray(chartRef.current.timestamp),
         labels: {
           show: false,
         },
       },
       yaxis: {
-        min: Math.min(...stockData.close) - 500,
-        max: Math.max(...stockData.close) + 100,
+        min: Math.min(...chartRef.current.close) - 500,
+        max: Math.max(...chartRef.current.close) + 100,
         opposite: true,
         tickAmount: 5,
       },
       title: {
-        text: stockData.exchange,
+        text: chartRef.current.exchange,
         align: "left",
       },
       subtitle: {
-        text: stockData.fullExchangeName,
+        text: chartRef.current.fullExchangeName,
         align: "left",
       },
     },
     series: [
       {
         name: "series-1",
-        data: stockData.close,
+        data: chartRef.current.close,
       },
     ],
   };
--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -1,49 +1,56 @@
-import { Row, Col, Button } from "react-bootstrap";
-import { authState } from "../../slice/auth/authSlice";
+import { Row, Col, Button, Form } from "react-bootstrap";
 import { useSelector, useDispatch } from "react-redux";
 import { dataState, yahooSummary } from "../../slice/data/dataSlice";
 import Summary from "../Charts/Summary";
+import { useEffect, useState } from "react";
 
 const Main = () => {
-  const auth = useSelector(authState);
   const data = useSelector(dataState);
   const dispatch = useDispatch();
-  const handleClick = () => {
-    console.log("click");
+
+  const [chartValue, setChartValue] = useState("DJI");
+  
+  useEffect(() => {
+    dispatchData();
+  }, [chartValue]);
+
+  const dispatchData = () => {
     dispatch(yahooSummary());
   };
+
+  const handleClick = (e) => {
+    const { value } = e.target;
+    setChartValue(value);
+  };
+
   return (
-    <>
-      {auth.isAuthenticated ? (
+    <Row>
+      <Col>
         <Row>
           <Col>
-            <Button onClick={handleClick}>Click</Button>
-            <Row>
-              {data.dataLoaded && (
-                <>
-                  {data.data.map((market, index) => {
-                    console.log("market.timstamp", market.timestamp);
-                    if (market.timestamp.length != 0) {
-                      return (
-                        <Col lg={4}>
-                          <Summary stockData={market} />
-                        </Col>
-                      );
-                    }
-                  })}
-                </>
-              )}
-            </Row>
+            <Form>
+              <Form.Select onChange={handleClick}>
+                <option value="DJI">DJI</option>
+                <option value="MEX">MEX</option>
+                <option value="OSA">OSA</option>
+                <option value="SAO">SAO</option>
+                <option value="BUE">BUE</option>
+                <option value="HKE">HKSE</option>
+              </Form.Select>
+            </Form>
           </Col>
         </Row>
-      ) : (
         <Row>
-          <Col>
-            <h1>Please log in to view this content!</h1>
-          </Col>
+          {data.dataLoaded && (
+            <>
+              <Col lg={4}>
+                <Summary stockData={chartValue} />
+              </Col>
+            </>
+          )}
         </Row>
-      )}
-    </>
+      </Col>
+    </Row>
   );
 };
 
