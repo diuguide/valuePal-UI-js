1fb1da304828ce013a93a77d9e702c54ffaac4a3
diuguide <everett.diuguid@gmail.com>
2021-10-15 16:51:29 -0400

    styled loggin logout

--- src/components/Nav/NavBar.jsx
+++ src/components/Nav/NavBar.jsx
@@ -1,25 +1,40 @@
 import { Navbar, Container, Nav } from "react-bootstrap";
+import { useSelector, useDispatch } from "react-redux";
 import { Link } from "react-router-dom";
+import { authState, logout } from "../../slice/auth/authSlice";
 import Login from "../Auth/Login";
 
 const NavBar = () => {
+  const auth = useSelector(authState);
+  const dispatch = useDispatch();
   const linkStyle = {
     textDecoration: "none",
     fontSize: "10px",
     margin: "2px",
-    color: "lightblue"
+    color: "lightblue",
   };
+
+  const handleLogout = () => {
+    dispatch(logout());
+  };
+
   return (
     <Navbar bg="dark" variant="dark">
       <Container>
         <Navbar.Brand href="/">ValuePal</Navbar.Brand>
-        <Nav className="d-flex justify-content-end">
-          <Login />
-          <div>
-            <Link style={linkStyle} to="/register">Register</Link>
-          <Link style={linkStyle} to="/logout">Logout</Link>
-          </div>
-          
+        <Nav className="d-flex justify-content-end align-items-center">
+          {!auth.isAuthenticated && <Login />}
+          {auth.isAuthenticated && (
+            <div style={linkStyle}>You are Logged In | </div>
+          )}
+          {!auth.isAuthenticated && (
+            <Link style={linkStyle} to="/register">
+              Register
+            </Link>
+          )}
+          <Link style={linkStyle} to="/" onClick={handleLogout}>
+            Logout
+          </Link>
         </Nav>
       </Container>
     </Navbar>
