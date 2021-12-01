da529fffdf5c0b33de56dd12faef8a1ed3a36c14
diuguide <everett.diuguid@gmail.com>
2021-12-01 15:03:33 -0500

    deleted unused components

--- src/components/Main/Welcome.jsx
+++ src/components/Main/Welcome.jsx
@@ -1,11 +1,9 @@
 import { Row, Col, Container } from "react-bootstrap";
 import { useSelector } from "react-redux";
 import { authState } from "../../slice/auth/authSlice";
-import TwitterFeed from "../Twitter";
 import TickerSearch from "../TickerSearch";
 import TickerResults from "../TickerSearch/Results";
 import { tickerDataState } from "../../slice/data/tickerSearchSlice";
-import { tickerHistoryState } from "../../slice/data/tickerHistorySlice";
 import HistChartWrapper from "../Charts/TickerHistory/Wrapper";
 import HoldingsTable from "../Wallet/HoldingsTable";
 import Wallet from "../Wallet/Wallet";
@@ -14,8 +12,7 @@ import PurchasePanelWrapper from "../Wallet/PurchasePanelWrapper";
 const Welcome = () => {
   const auth = useSelector(authState);
   const tickerData = useSelector(tickerDataState);
-  // const tickerHistory = useSelector(tickerHistoryState);
-  // const twitterHandle = ["DowJones", "Nasdaq", "CMEGroup"];
+  
   return (
     <Container>
       <Row className="bg-light">
@@ -36,16 +33,6 @@ const Welcome = () => {
           </Col>
         </Row>
       )}
-
-      {/* <Row className="d-flex justify-content-center">
-        {twitterHandle.map((tw, index) => {
-          return (
-            <Col key={index}>
-              <TwitterFeed twitterHandle={tw} />;
-            </Col>
-          );
-        })}
-      </Row> */}
     </Container>
   );
 };
