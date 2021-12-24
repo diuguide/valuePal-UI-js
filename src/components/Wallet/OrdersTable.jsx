ae662db7f301f3b076781a01d932e7381526d3d3
diuguide <everett.diuguid@gmail.com>
2021-12-23 23:52:07 -0500

    populated order table with orders, added decimal formatting

--- src/components/Wallet/OrdersTable.jsx
+++ src/components/Wallet/OrdersTable.jsx
@@ -2,6 +2,7 @@ import { Row, Col, Table } from "react-bootstrap";
 import { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { userOrders, walletState } from "../../slice/wallet/walletSlice";
+import Loader from "../Loader/Loader";
 
 const OrdersTable = () => {
   const dispatch = useDispatch();
@@ -15,25 +16,43 @@ const OrdersTable = () => {
     console.log("orders table render!");
     getOrders();
   }, []);
-  
 
   return (
     <>
-      <Row>
-        <Col>
-          <Table striped bordered hover>
-            <thead>
-              <tr>
-                <th>Type</th>
-                <th>Ticker</th>
-                <th>Price</th>
-                <th>Quantity</th>
-                <th>Status</th>
-              </tr>
-            </thead>
-          </Table>
-        </Col>
-      </Row>
+      {order.order.isLoaded ? (
+        <Row>
+          <Col>
+            <Table striped bordered hover>
+              <thead>
+                <tr>
+                  <th>Type</th>
+                  <th>Ticker</th>
+                  <th>Price</th>
+                  <th>Quantity</th>
+                  <th>Cost</th>
+                  <th>Status</th>
+                </tr>
+              </thead>
+              <tbody>
+                {order.order.orders.map((order, index) => {
+                  return (
+                    <tr key={index}>
+                      <td>{order.orderType}</td>
+                      <td>{order.ticker}</td>
+                      <td>{order.price.toFixed(2)}</td>
+                      <td>{order.quantity}</td>
+                      <td>{order.totalValue.toFixed(2)}</td>
+                      <td>{order.status}</td>
+                    </tr>
+                  );
+                })}
+              </tbody>
+            </Table>
+          </Col>
+        </Row>
+      ) : (
+        <Loader />
+      )}
     </>
   );
 };
