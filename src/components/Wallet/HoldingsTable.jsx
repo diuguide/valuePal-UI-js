b85a744303e24180c334a7a09aed34c7917e7910
diuguide <everett.diuguid@gmail.com>
2022-04-08 18:52:44 -0400

    holdings table change cell component created inside of holdings table component, mostly straightforward styling.

--- src/components/Wallet/HoldingsTable.jsx
+++ src/components/Wallet/HoldingsTable.jsx
@@ -27,13 +27,58 @@ const HoldingsTable = () => {
     quantity: {},
     price: {},
     avg_price: {},
-    change: {
-      
-      fontWeight: 900,
-    },
+    change: {},
     totalValue: {
       fontSize: "32px",
     },
+  };
+
+  const ChangeCell = ({ price, avgPrice }) => {
+    const style = {
+      container: {
+        display: "flex",
+        justifyContent: "space-around"
+      },
+      left: {
+        marginTop: "10px",
+        display: "block",
+      },
+      right: {
+        display: "block",
+      },
+      dollar: {
+        color: price - avgPrice >= 0 ? "green" : "red",
+      },
+      percentage: {
+        color: price - avgPrice >= 0 ? "green" : "red",
+      },
+      arrow: {
+        fontSize: "56px",
+        fontWeight: "900",
+        color: price - avgPrice <= 0 ? "red" : "green",
+      },
+    };
+    return (
+      <>
+        <div style={style.container}>
+          <div style={style.left}>
+            <div style={style.dollar}>$ {(price - avgPrice).toFixed(2)}</div>
+            <div style={style.percentage}>
+              {(((price - avgPrice) / avgPrice) * 100).toFixed(2)}%
+            </div>
+          </div>
+          <div style={style.right}>
+          <div style={style.arrow}>
+            {price - avgPrice <= 0 ? (
+              <div>&#8595;</div>
+            ) : (
+              <div>&#8593;</div>
+            )}
+          </div>
+          </div>
+        </div>
+      </>
+    );
   };
 
   return (
@@ -60,10 +105,22 @@ const HoldingsTable = () => {
                         <tr key={index}>
                           <td style={rowStyle.ticker}>{ticker.ticker}</td>
                           <td style={rowStyle.quantity}>{ticker.quantity}</td>
-                          <td style={rowStyle.price}>{ticker.price.toFixed(2)}</td>
-                          <td style={rowStyle.avg_price}>{ticker.avgPrice.toFixed(2)}</td>
-                          <td style={rowStyle.change}>{(ticker.price - ticker.avgPrice).toFixed(2)}</td>
-                          <td style={rowStyle.totalValue}>{(ticker.price * ticker.quantity).toFixed(2)}</td>
+                          <td style={rowStyle.price}>
+                            {ticker.price.toFixed(2)}
+                          </td>
+                          <td style={rowStyle.avg_price}>
+                            {ticker.avgPrice.toFixed(2)}
+                          </td>
+                          <td style={rowStyle.change}>
+                            <ChangeCell
+                              price={ticker.price}
+                              avgPrice={ticker.avgPrice}
+                            />
+                          </td>
+                          {/* <td style={rowStyle.change}>{(ticker.price - ticker.avgPrice).toFixed(2)}</td> */}
+                          <td style={rowStyle.totalValue}>
+                            {(ticker.price * ticker.quantity).toFixed(2)}
+                          </td>
                         </tr>
                       );
                     })
