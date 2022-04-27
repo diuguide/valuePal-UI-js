1ce3192191b8219a5063e6673e405ff50fea3679
diuguide <everett.diuguid@gmail.com>
2022-04-26 20:47:49 -0400

    created landing directory.  rerouting all site traffic past a welcome page and register, with some instructions

--- src/components/Auth/index.js
+++ src/components/Auth/index.js
@@ -2,10 +2,13 @@ import { Row, Col, Form, Button, Alert } from "react-bootstrap";
 import { registerUser } from "../../utilities/auth";
 import { useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
-import { errorState, showMessage, hideMessage } from "../../slice/error/errorSlice";
+import {
+  errorState,
+  showMessage,
+  hideMessage,
+} from "../../slice/error/errorSlice";
 
 const Register = () => {
-
   const error = useSelector(errorState);
   const dispatch = useDispatch();
 
@@ -14,8 +17,8 @@ const Register = () => {
     password: "",
     firstName: "",
     email: "",
-    role:"ROLE_USER",
-    watchlist: "AMC,GME,AAPL"
+    role: "ROLE_USER",
+    watchlist: "AMC,GME,AAPL",
   });
 
   const handleChange = (e) => {
@@ -31,8 +34,8 @@ const Register = () => {
           password: "",
           firstName: "",
           email: "",
-          role:"ROLE_USER",
-          watchlist: "AMC,GME,AAPL"
+          role: "ROLE_USER",
+          watchlist: "AMC,GME,AAPL",
         });
       })
       .catch((err) => {
@@ -44,8 +47,8 @@ const Register = () => {
   };
 
   return (
-    <Row>
-      <Col>
+    <Row className="d-flex">
+      <Col className="col-md-6 col-lg-6 col-sm-9">
         <Form>
           <Form.Group className="m-2">
             <Form.Control
@@ -83,9 +86,11 @@ const Register = () => {
               placeholder="Password"
             ></Form.Control>
           </Form.Group>
-          {error.showMsg &&
-          <Alert className="m-2" variant="danger">{error.msg}</Alert>
-          }
+          {error.showMsg && (
+            <Alert className="m-2" variant="danger">
+              {error.msg}
+            </Alert>
+          )}
           <Button className="m-2" onClick={handleClick}>
             Register
           </Button>
---
+++ src/components/Landing/Main.jsx
@@ -0,0 +1,13 @@
+import { Row, Col } from 'react-bootstrap';
+
+const LandingMain = () => {
+    return(
+        <Row>
+            <Col>
+            <div>Test</div>
+            </Col>
+        </Row>
+    )
+}
+
+export default LandingMain;
